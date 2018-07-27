e1ca1322515f33ed985302ed89af1c2ac8864f8e
MrFlutters <flutters@fluttershub.xyz>
2018-07-27 05:53:08 +0100

    Changed chrome

--- testem.js
+++ testem.js
@@ -2,7 +2,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'google-chrome-stable'
+    'google-chrome'
   ],
   launch_in_dev: [
     'Chrome'
